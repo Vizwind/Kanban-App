31ed87e79a1cb54c53ccee5f39fa42b63957397e
Roland Lu <roland.lu@microbenefits.com>
2017-04-18 19:05:22 +0800

    almost finish chapter 8

--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,38 @@ const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const NpmInstallPlugin = require('npm-install-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+const pkg = require('./package.json');
 const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
-  build: path.join(__dirname, 'build')
+  build: path.join(__dirname, 'build'),
+  style: path.join(__dirname, 'app/main.css')
 };
 
 process.env.BABEL_ENV = TARGET;
 
 const common = {
-// Entry accepts a path or an object of entries. We'll be using the
-// latter form given it's convenient with more complex configurations.
   entry: {
-    app: PATHS.app
+    app: PATHS.app,
+    style: PATHS.style
   },
   resolve: {
     extensions: ['*', '.js', '.jsx']
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: '[name].js',
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         enforce: 'pre',
-// Enable caching for improved performance during development
-// It uses default OS directory by default. If you need something
-// more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
         loaders: ['babel-loader?cacheDirectory'],
-// Parse only app files! Without this it will go through entire project.
-// In addition to being slow, that will most likely result in an error.
         include: PATHS.app
       },
       {
@@ -43,40 +42,37 @@ const common = {
         loaders: ['eslint-loader'],
         include: PATHS.app
       },
-      {
-        // Test expects a RegExp! Note the slashes!
-        test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
-        // Include accepts either a path or an array of paths.
-        include: PATHS.app
-      }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'node_modules/html-webpack-template/index.ejs',
+      title: 'Kanban app',
+      appMountId: 'app',
+      inject: false
+    })
+  ]
 };
 
-// Default configuration
 if (TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
     devtool: 'eval-source-map',
     devServer: {
-      contentBase: PATHS.build,
-// Enable history API fallback so HTML5 History API based
-// routing works. This is a good default that will come
-// in handy in more complicated setups.
       historyApiFallback: true,
       hot: true,
       inline: true,
-// Display only errors to reduce the amount of output.
       stats: 'errors-only',
-// Parse host and port from env so this is easy to customize.
-//
-// If you use Vagrant or Cloud9, set
-// host: process.env.HOST || '0.0.0.0';
-//
-// 0.0.0.0 is available to all network devices unlike default
-// localhost
       host: process.env.HOST,
       port: process.env.PORT
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+          include: PATHS.app
+        }
+      ]
     },
     plugins: [
       new webpack.HotModuleReplacementPlugin(),
@@ -87,6 +83,41 @@ if (TARGET === 'start' || !TARGET) {
   });
 }
 
-if (TARGET === 'build') {
-  module.exports = merge(common, {});
+if (TARGET === 'build' || TARGET === 'stats') {
+  module.exports = merge(common, {
+    entry: {
+      vendor: Object.keys(pkg.dependencies).filter(function (v) {
+        return v !== 'alt-utils';
+      })
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }),
+          include: PATHS.app
+        }
+      ]
+    },
+    plugins: [
+      new CleanPlugin([PATHS.build]),
+      new ExtractTextPlugin('[name].[chunkhash].css'),
+      new webpack.optimize.CommonsChunkPlugin({
+        names: ['vendor', 'manifest']
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"'
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  });
 }
